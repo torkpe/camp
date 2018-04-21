0565152bd70f8ddddc5c54ad6f57466db86ead7d
Temitope Emmanuel <temitope.emmanuel63@gmail.com>
2018-04-21 17:33:22 +0100

    mongo

--- Public/script/background.js
+++ Public/script/background.js
@@ -1,7 +1,6 @@
 var counter = 0;
 function changeBG(){
     var imgs = [
-        "url(http://localhost:5000/uploads/1524305002971.jpg)",
         "url(https://images.unsplash.com/photo-1444012236767-1b471c68781c?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
         "url(https://images.unsplash.com/photo-1444012104069-996724bf4a0a?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
         "url(https://images.unsplash.com/photo-1445888761652-fc13cbb0d819?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
